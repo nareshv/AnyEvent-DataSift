46500d4714818c0cb370bcc56bf067e64982f8b1
niko <nikomomo@gmail.com>
2012-06-01 17:42:10 +0200

    http parameters fixed

--- lib/AnyEvent/DataSift.pm
+++ lib/AnyEvent/DataSift.pm
@@ -7,9 +7,10 @@ use warnings;
 use Carp;
 use AnyEvent::HTTP;
 use AnyEvent::DataSift::Stream::HTTP;
+use URI;
 use JSON;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub new {
 	my $class = shift;
@@ -37,18 +38,38 @@ sub REST {
 		api_key  => $self->{apikey},
 		@_,
 	};
-	my $on_data  = delete($params->{on_data}) || sub{};
+	my $on_data  = delete($params->{on_data})  || sub{};
 	my $on_error = delete($params->{on_error}) || sub{};
-	my $method   = delete($params->{method}) || 'POST';
+	my $method   = delete($params->{method})   || 'POST';
+
+	my $body;
+	my $headers = {
+		'User-Agent'  => $self->{useragent},
+	};
+	my $uri = URI->new( 'http://api.'.$self->{host}.':'.$self->{port}.'/'.$api );
+	if( $method eq 'POST' ){
+		$body = join '&',
+			map $_.'='.URI::Escape::uri_escape($params->{$_}),
+			keys %$params;
+		$headers->{'Content-Type'} = 'application/x-www-form-urlencoded';
+	} else {
+		$uri->query_form( $self->{params} );
+	}
+
 	http_request 
-		$method  => 'http://api.'.$self->{host}.'/'.$api,
-		%$params,
+		$method  => $uri,
+		timeout  => $self->{timeout},
+		headers  => $headers,
+		body     => $body,
 		sub {
 			my( $data, $headers ) = @_;
 			if ($headers->{Status} =~ /^2/) {
 				$on_data->( decode_json $data );
 			} else {
-				$on_error->( $headers->{Status}, $headers->{Reason} );
+				$on_error->(
+					$headers->{Status}, 
+					eval { decode_json($data)->{error} } || $headers->{Reason}
+				);
 			}
 		};
 	$self;
